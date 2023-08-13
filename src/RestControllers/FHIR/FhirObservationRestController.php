edb5a10214dd89fc0bde3ee8bc944830ae581da7
saidabdul80 <saidabdulsalam5@gmail.com>
2023-08-13 18:57:06 +0100

    fix: updates

---
+++ src/RestControllers/FHIR/FhirObservationRestController.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * FhirObservationRestController
+ *
+ * @package   OpenEMR
+ * @link      http://www.open-emr.org
+ * @author    Yash Bothra <yashrajbothra786@gmail.com>
+ * @copyright Copyright (c) 2020 Yash Bothra <yashrajbothra786@gmail.com>
+ * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
+ */
+
+namespace OpenEMR\RestControllers\FHIR;
+
+use OpenEMR\Services\FHIR\FhirObservationService;
+use OpenEMR\Services\FHIR\FhirResourcesService;
+use OpenEMR\RestControllers\RestControllerHelper;
+use OpenEMR\FHIR\R4\FHIRResource\FHIRBundle\FHIRBundleEntry;
+use OpenEMR\Services\FHIR\FhirValidationService;
+class FhirObservationRestController
+{
+    private $fhirObservationService;
+    private $fhirService;
+    private $fhirValidationService;
+    public function __construct()
+    {
+        $this->fhirObservationService = new FhirObservationService();
+        $this->fhirService = new FhirResourcesService();
+        $this->fhirValidationService = new FhirValidationService();
+    }
+
+    /**
+     * Queries for a single FHIR observation resource by FHIR id
+     * @param $fhirId The FHIR observation resource id (uuid)
+     * @param $puuidBind - Optional variable to only allow visibility of the patient with this puuid.
+     * @returns 200 if the operation completes successfully
+     */
+    public function getOne($fhirId, $puuidBind = null)
+    {
+        $processingResult = $this->fhirObservationService->getOne($fhirId, $puuidBind);
+        return RestControllerHelper::handleFhirProcessingResult($processingResult, 200);
+    }
+
+    /**
+     * Queries for FHIR observation resources using various search parameters.
+     * Search parameters include:
+     * - patient (puuid)
+     * @param $puuidBind - Optional variable to only allow visibility of the patient with this puuid.
+     * @return FHIR bundle with query results, if found
+     */
+    public function getAll($searchParams, $puuidBind = null)
+    {
+        $processingResult = $this->fhirObservationService->getAll($searchParams, $puuidBind);
+        $bundleEntries = array();
+        foreach ($processingResult->getData() as $index => $searchResult) {
+            $bundleEntry = [
+                'fullUrl' =>  $GLOBALS['site_addr_oath'] . ($_SERVER['REDIRECT_URL'] ?? '') . '/' . $searchResult->getId(),
+                'resource' => $searchResult
+            ];
+            $fhirBundleEntry = new FHIRBundleEntry($bundleEntry);
+            array_push($bundleEntries, $fhirBundleEntry);
+        }
+        $bundleSearchResult = $this->fhirService->createBundle('Observation', $bundleEntries, false);
+        $searchResponseBody = RestControllerHelper::responseHandler($bundleSearchResult, null, 200);
+        return $searchResponseBody;
+    }
+
+    public function post($fhirJson)
+    {
+        $fhirValidationService = $this->fhirValidationService->validate($fhirJson);
+        if (!empty($fhirValidationService)) {
+            return RestControllerHelper::responseHandler($fhirValidationService, null, 400);
+        }
+
+        $organization = $this->createObservationFromJSON($fhirJson);
+        $processingResult = $this->fhirOrganizationService->insert($organization);
+        return RestControllerHelper::handleFhirProcessingResult($processingResult, 201);
+    }
+}
